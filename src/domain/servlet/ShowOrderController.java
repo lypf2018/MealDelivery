b7359ea723c05ad68428e42d79aed5b6322d46db
Superbbbb <chloesoo.superb@gmail.com>
2018-11-21 19:00:13 -0600

    showOrder

--- src/domain/servlet/ShowOrderController.java
+++ src/domain/servlet/ShowOrderController.java
@@ -15,15 +15,15 @@ import domain.dao.AdminOrderDao;
 /**
  * Servlet implementation class showOrder
  */
-@WebServlet("/admin/showOrder")
-public class showOrder extends HttpServlet {
+@WebServlet("/admin/ShowOrder")
+public class ShowOrderController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public showOrder() {
+    public ShowOrderController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,10 +32,24 @@ public class showOrder extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//get the id value of query string
+		String id = request.getParameter("id");
+//		String status = request.getParameter("status");
 		AdminOrderDao orderDao = new AdminOrderDao();
-		ArrayList<Order> list = orderDao.displayOrder();
-		request.setAttribute("list", list);
-		request.getRequestDispatcher("/admin/adminShowOrder.jsp").forward(request, response);
+		if(id.equals("all")) {
+			ArrayList<Order> list = orderDao.displayOrder();
+			request.setAttribute("list", list);
+			request.getRequestDispatcher("/admin/adminShowOrder.jsp").forward(request, response);
+		}
+		else {
+			Order order = orderDao.getOrder(id);
+			request.setAttribute("id", id);
+			request.setAttribute("order", order);
+//			request.setAttribute("status", status);
+			request.getRequestDispatcher("/admin/getOrderDetail.jsp").forward(request, response);
+//			response.sendRedirect(request.getContextPath() + "/admin/getOrderDetail.jsp");
+		}
+		
 	}
 
 	/**
@@ -43,7 +57,11 @@ public class showOrder extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		AdminOrderDao orderDao = new AdminOrderDao();
+		String status = request.getParameter("status");
+		String id = (String)request.getAttribute("orderId");
+		System.out.println(id);
+		//orderDao.updateStatus();
 	}
 	
 //	public static void main(Stirng[] args) {
